2007efadb4abfba462965506eed9068f2cb91825
arinvern <69636668+arinvern@users.noreply.github.com>
2023-05-07 17:16:34 +0200

    Add files via upload

---
+++ S04T02N01InvernonMateoAriadna/src/main/java/cat/itacademy/barcelonactiva/invernon/mateo/ariadna/s04/S04T02N01InvernonMateoAriadnaApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.invernon.mateo.ariadna.s04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T02N01InvernonMateoAriadnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T02N01InvernonMateoAriadnaApplication.class, args);
+	}
+
+}
